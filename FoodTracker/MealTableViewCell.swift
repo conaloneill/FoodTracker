d5662105efa681810ae2cc942784a5bad0b3e42c
Conal O'Neill <conal.oneill95@gmail.com>
2017-12-04 04:16:53 +0000

    Initial Commit

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackerTest
+//
+//  Created by Conal O'Neill on 04/12/2017.
+//  Copyright © 2017 Conal O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+	
+	//MARK: Properties
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var photoImageView: UIImageView!
+	@IBOutlet weak var ratingControl: RatingControl!
+	
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
